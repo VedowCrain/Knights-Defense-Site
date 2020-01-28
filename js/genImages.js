558b40263763abfbf60bdbe53bf2838f5b2f1a30
VedowCrain <Calvinvdwesthuizen@gmail.com>
2020-01-28 16:04:48 +0200

    Java Script :smile:

---
+++ js/genImages.js
@@ -0,0 +1,15 @@
+var imgNums = [...Array(29).keys()].slice(1);
+
+console.log("gen");
+
+function imgGen() {
+  var flipbook = document.getElementById("flipbook");
+  imgNums.forEach(num => {
+    var element = document.createElement("div");
+    element.style.background = `url(./assets/knight_Defense/kd_Doc_${num}.jpg)`;
+    element.className = "page";
+    flipbook.appendChild(element);
+  });
+}
+
+imgGen();
